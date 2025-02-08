157dafba22a2e25c15bfb42776343941795f7593
Samily <samily.sss2005@gmail.com>
2025-02-07 21:39:31 -0300

    tarefas adicionadas

--- script.js
+++ script.js
@@ -11,10 +11,47 @@ const tasks = [
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
 
-function renderElements (tasks) {
-  const lista = document.querySelector('ul');
+function renderElements(tasks) {
+  const taskList = document.querySelector("ul");
+  taskList.innerHTML = "";
+
+  tasks.forEach(task => {
+    const taskItem = createTaskItem(task); // Cria o elemento <li>
+    taskList.appendChild(taskItem); // Adiciona à <ul>
+  });
+}
+renderElements(tasks);
+
+function createTaskItem(task) {
+  const li = document.createElement('li');
+  li.classList.add('task__item');
+
+  const div = document.createElement('div');
+  div.classList.add('task-info__container');
+
+  const span = document.createElement('span');
+  span.classList.add('task-type');
+  
+  if (task.type === 'Urgente') {
+    span.classList.add('span-urgent');
+  } else if (task.type === 'Importante') {
+    span.classList.add('span-important');
+  } else if (task.type === 'Normal') {
+    span.classList.add('span-normal');
+  }
+
+  const p = document.createElement('p');
+  p.textContent = task.title;
+
+  div.appendChild(span);
+  div.appendChild(p);
+
+  const button = document.createElement('button');
+  button.classList.add('task__button--remove-task');
+
+  li.appendChild(div);
+  li.appendChild(button);
+
+  return li;
 }
 
-function createTaskItem (title, type) {
-  
-}
